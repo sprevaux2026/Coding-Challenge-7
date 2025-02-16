23a688badfc79990aad9be57ee931a4614420f83
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-16 09:32:15 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -7,3 +7,13 @@ function calculateInvoice(subtotal, taxRate, discount) {
 // Test Data and console logging
 console.log(`Total Invoice: $${calculateInvoice(100, 0.08, 5).toFixed(2)}`);
 console.log(`Total Invoice: $${calculateInvoice(500, 0.1, 20).toFixed(2)}`);
+
+// Task 2: Function Expression - Employee Hourly Wage Calculation
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52);
+    return hourlyWage;
+};
+
+// Test Data and logging
+console.log(`Hourly Wage: $${calculateHourlyWage(52000, 40).toFixed(2)}`);
+console.log(`Hourly Wage: $${calculateHourlyWage(75000, 35).toFixed(2)}`);
