69133d33741f14d1cc8b1e44a330e2f73d79e15d
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-16 09:33:05 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -17,3 +17,22 @@ const calculateHourlyWage = function(salary, hoursPerWeek) {
 // Test Data and logging
 console.log(`Hourly Wage: $${calculateHourlyWage(52000, 40).toFixed(2)}`);
 console.log(`Hourly Wage: $${calculateHourlyWage(75000, 35).toFixed(2)}`);
+
+// Task 3: Arrow Function - Customer Loyalty Discount
+//calulate discount based on years of loyalty
+const calculateLoyaltyDiscount = (amount, years) => {
+    let discountRate = 0;
+    if (years >= 5) {
+        discountRate = 0.15;
+    } else if (years >= 3) {
+        discountRate = 0.10;
+    } else {
+        discountRate = 0.05;
+    }
+    let discountedPrice = amount - (amount * discountRate);
+    return discountedPrice;
+};
+
+// Test Data and logging
+console.log(`Discounted Price: $${calculateLoyaltyDiscount(100, 6).toFixed(2)}`);
+console.log(`Discounted Price: $${calculateLoyaltyDiscount(200, 2).toFixed(2)}`);
