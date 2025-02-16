7fb327cd665690299c8020a23c0888923820df12
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-16 09:33:47 -0500

    Task 4 - Product Shipping Cost Calculation

--- cc_7.js
+++ cc_7.js
@@ -36,3 +36,26 @@ const calculateLoyaltyDiscount = (amount, years) => {
 // Test Data and logging
 console.log(`Discounted Price: $${calculateLoyaltyDiscount(100, 6).toFixed(2)}`);
 console.log(`Discounted Price: $${calculateLoyaltyDiscount(200, 2).toFixed(2)}`);
+
+// Task 4: Parameters and Arguments - Product Shipping Cost Calculation
+//calculate shipping cost based on weight, location and expedited shipping
+function calculateShippingCost(weight, location, expedited = false) {
+    let baseFee = 0;
+    let perLb = 0;
+    if (location === "USA") {
+        baseFee = 5;
+        perLb = 0.5;
+    } else if (location === "Canada") {
+        baseFee = 10;
+        perLb = 0.7;
+    }
+    let shippingCost = baseFee + (perLb * weight);
+    if (expedited) {
+        shippingCost += 10;
+    }
+    return shippingCost;
+}
+
+// Test Data and logging
+console.log(`Shipping Cost: $${calculateShippingCost(10, "USA", true).toFixed(2)}`);
+console.log(`Shipping Cost: $${calculateShippingCost(5, "Canada", false).toFixed(2)}`);
